26c14ede853afc1cb94e9da3918767308dcdb206
akhil683 <onkar.palsra@gmail.com>
2024-11-11 22:37:15 +0530

    feat: added clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="text-4xl">Hello</div>;
+  return (
+    <div className="bg-gradient-to-r min-h-screen from-rose-100 to-indigo-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-semibold text-7xl text-center ">
+          AT
+          <span className="text-green-600 font-bold">
+            note taking
+          </span>
+          assistant.
+        </h1>
+        <div className="mt-4">
+          <h2 className="font-semibold text-3xl text-center text-slate-700">
+            AI powered
+          </h2>
+        </div>
+      </div>
+      helllo
+      <Button>Hello</Button>
+    </div>
+
+  )
 }
---
+++ src/components/TypewriterTitle.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+//
+// type Props = {}
+//
+// const TypewriteTitle = (props: Props) => {
+//   return (
+//     <div>TypewriteTitle</div>
+//   )
+// }
+//
+// export default TypewriteTitle
